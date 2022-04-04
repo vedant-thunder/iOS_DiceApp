68374cd773156b82b331670c48aecfdbb605a001
VEDANT KHANNA <89499026+vedant-thunder@users.noreply.github.com>
2022-04-04 22:19:56 +0530

    Add files via upload
    An ios app with some dice rolling. Designed with swift.

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,17 @@
+//  ViewController.swift
+//  Dicee-iOS13
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let  dicearray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image = dicearray.randomElement()
+        diceImageView2.image = dicearray.randomElement()
+        
+       
+    }
+
+}
